a2bdaddde8d9749addbeaeaf1e614569c192c424
UdaySidhu625 <sidhuudaypartapsingh@gmail.com>
2022-10-29 15:29:22 -0400

    Midterm Complete
    Completed Midterm

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// Do not expose your credentials in your code.
+let atlasDB = "mongodb+srv://UdaySidhu:1BuWGG6n4954hJ1i@cluster0.gp8wx4z.mongodb.net/test";
+
+// Database setup
+let mongoose = require('mongoose');
+
+module.exports = function(){
+
+    mongoose.connect(atlasDB);
+    let mongodb = mongoose.connection;
+
+    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
+    mongodb.once('open', ()=>{
+        console.log('===> Connected to MongoDB.');
+    })
+
+    return mongodb;
+}
---
+++ controllers/todo.js
@@ -0,0 +1,157 @@
+// create a reference to the model
+let TodoModel = require('../models/todo');
+
+// Gets all todo from the Database and renders the page to list them all.
+module.exports.todoList = function(req, res, next) {  
+
+    TodoModel.find((err, todoList) => {
+        //console.log(todoList);
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            res.render('todo/list', {
+                title: 'To-Do List', 
+                TodoList: todoList,
+                userName: req.user ? req.user.username : ''
+            })            
+        }
+    });
+}
+
+
+// Gets a todo by id and renders the details page.
+module.exports.details = (req, res, next) => {
+    
+    let id = req.params.id;
+
+    TodoModel.findById(id, (err, todoToShow) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('todo/details', {
+                title: 'To-Do Details', 
+                todo: todoToShow
+            })
+        }
+    });
+}
+
+// Gets a todo by id and renders the Edit form using the add_edit.ejs template
+module.exports.displayEditPage = (req, res, next) => {
+    
+    let id = req.params.id;
+
+    TodoModel.findById(id, (err, itemToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            
+            res.render('todo/add_edit', {
+                title: 'Item Edit', 
+                todo: itemToEdit
+            })
+        }
+    });
+
+}
+
+// Processes the data submitted from the Edit form to update a todo
+module.exports.processEditPage = (req, res, next) => {
+
+    let id = req.params.id
+    
+    console.log(req.body);
+
+    let updatedTodo = TodoModel({
+        _id: req.body.id,
+        task: req.body.task,
+        description: req.body.description,
+        complete: req.body.complete ? true : false
+    });
+
+   
+
+    TodoModel.updateOne({_id: id}, updatedTodo, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            
+            res.redirect('/todo/list');
+        }
+    });
+
+}
+
+// Deletes a todo based on its id.
+module.exports.performDelete = (req, res, next) => {
+
+    let id = req.params.id;
+
+    TodoModel.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/todo/list');
+        }
+    });
+
+}
+
+// Renders the Add form using the add_edit.ejs template
+module.exports.displayAddPage = (req, res, next) => {
+
+    let newItem = TodoModel();
+
+    res.render('todo/add_edit', {
+        title: 'Add Todo Info',
+        todo: newItem
+    })           
+
+}
+
+// Processes the data submitted from the Add form to create a new todo
+module.exports.processAddPage = (req, res, next) => {
+
+    console.log(req.body);
+
+    let newTodo = TodoModel({
+        _id: req.body.id,
+        task: req.body.task,
+        description: req.body.description,
+        complete: req.body.complete ? true : false
+    });
+
+    TodoModel.create(newTodo, (err, item) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            console.log(item);
+            res.redirect('/todo/list');
+        }
+    });
+    
+}
